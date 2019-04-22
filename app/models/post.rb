f66538785a4780f661980ae2014f76b301e42c3f
Anson Lam <anson.lam94@gmail.com>
2019-04-21 19:57:59 -0700

    Create post model with validations

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ApplicationRecord
+    validates(
+        :title, 
+        presence: true,
+        uniqueness: true
+    )
+
+    validates(
+        :body, 
+        length: { minimum: 50 }
+    )
+end

0f0853687baaa54dc404e5383b49daf9f9c9c573
Anson Lam <anson.lam94@gmail.com>
2019-04-27 18:52:26 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Comment.delete_all
+Post.delete_all
+
+50.times do
+    created_at = Faker::Date.backward(365 * 5)
+  p = Post.create(
+    title: Faker::Nation.capital_city,
+    body: Faker::Lorem.paragraph(2),
+    created_at: created_at,
+    updated_at: created_at
+  )
+  if p.valid?
+    p.comments = rand(0..15).times.map do
+      Comment.new(body: Faker::Hipster.sentence)
+    end
+end
+end
+
+posts = Post.all
+comments = Comment.all
+
+puts Cowsay.say("Generated #{ posts.count } posts", :ghostbusters)
+puts Cowsay.say("Generated #{ comments.count } comments", :stegosaurus)
